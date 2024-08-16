2d74d43d80c60c6cc862caa53cfcb5b9fe612f81
HidayatLahabu <hidayatdevacc@gmail.com>
2024-08-16 14:03:56 +0800

    remove antrian ruangan

--- public/build/assets/Dashboard-iTZXYJEy.js
+++
@@ -1,1 +0,0 @@
-import{j as r,Y as e}from"./app-DW9_4O4t.js";import{A as t}from"./AuthenticatedLayout-BqparqR8.js";import o from"./DashboardTable-DASI0w09.js";import"./transition-CZGNBuh4.js";function x({auth:s,items:a}){return r.jsxs(t,{user:s.user,children:[r.jsx(e,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(o,{items:a})})]})}export{x as default};
