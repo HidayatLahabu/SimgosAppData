3c87383f2dcd8858c4eff963b54bde7b502f976e
HidayatLahabu <hidayatdevacc@gmail.com>
2024-08-20 11:34:19 +0800

    update pengajuan rekon

--- public/build/assets/Dashboard-BlVPC6DW.js
+++
@@ -1,1 +0,0 @@
-import{j as r,Y as e}from"./app-DBlS62vo.js";import{A as t}from"./AuthenticatedLayout-aIlQPjjB.js";import o from"./DashboardTable-D6Ocuusn.js";import"./transition-fVzqlMaZ.js";function x({auth:s,items:a}){return r.jsxs(t,{user:s.user,children:[r.jsx(e,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(o,{items:a})})]})}export{x as default};
