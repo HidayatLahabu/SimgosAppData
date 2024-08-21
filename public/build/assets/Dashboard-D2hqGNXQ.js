e4e4ef86a5623127bbe7a4bc8f15492ced08e396
HidayatLahabu <hidayatdevacc@gmail.com>
2024-08-21 12:32:50 +0800

    update layanan order lab

--- public/build/assets/Dashboard-D2hqGNXQ.js
+++
@@ -1,1 +0,0 @@
-import{j as r,Y as e}from"./app-DNaRh_3e.js";import{A as t}from"./AuthenticatedLayout--DITQZAO.js";import o from"./DashboardTable-kaJ6OcX5.js";import"./transition-DYKI6Dgj.js";function x({auth:s,items:a}){return r.jsxs(t,{user:s.user,children:[r.jsx(e,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(o,{items:a})})]})}export{x as default};
