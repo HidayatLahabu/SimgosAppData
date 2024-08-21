3cd08f8cf3affda3b0c05814728c2e52bfa24946
HidayatLahabu <hidayatdevacc@gmail.com>
2024-08-21 17:58:01 +0800

    add logs

---
+++ app/Http/Controllers/Logs/PenggunaAksesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Logs;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PenggunaAksesController extends Controller
+{
+    //
+}
