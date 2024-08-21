62b7dfdccea18b891a7a2a8483de334780c99731
HidayatLahabu <hidayatdevacc@gmail.com>
2024-08-21 11:47:55 +0800

    add layanan order lab

--- app/Http/Controllers/Layanan/LaboratoriumController.php
+++ app/Http/Controllers/Layanan/LaboratoriumController.php
@@ -3,9 +3,47 @@
 namespace App\Http\Controllers\Layanan;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class LaboratoriumController extends Controller
 {
-    //
+    public function index()
+    {
+        // Get the search term from the request
+        $searchSubject = request('nama') ? strtolower(request('nama')) : null;
+
+        // Start building the query using the query builder
+        $query = DB::connection('mysql6')->table('layanan.order_lab as orderLab')
+            ->select(
+                'orderLab.NOMOR as nomor',
+                'orderLab.TANGGAL as tanggal',
+                'orderLab.KUNJUNGAN as kunjungan',
+                'pasien.norm',
+                'pasien.nama'
+            )
+            ->leftJoin('pendaftaran.kunjungan as kunjungan', 'kunjungan.NOMOR', '=', 'orderLab.KUNJUNGAN')
+            ->leftJoin('pendaftaran.pendaftaran as pendaftaran', 'pendaftaran.NOMOR', '=', 'kunjungan.NOPEN')
+            ->leftJoin('master.pasien as pasien', 'pasien.NORM', '=', 'pendaftaran.NORM');
+
+        // Add search filter if provided
+        if ($searchSubject) {
+            $query->whereRaw('LOWER(peserta.nama) LIKE ?', ['%' . $searchSubject . '%']);
+        }
+
+        // Paginate the results
+        $data = $query->orderByDesc('orderLab.TANGGAL')->paginate(10)->appends(request()->query());
+
+        // Convert data to array
+        $dataArray = $data->toArray();
+
+        // Return Inertia view with paginated data
+        return inertia("Layanan/Laboratorium/Index", [
+            'layananLab' => [
+                'data' => $dataArray['data'], // Only the paginated data
+                'links' => $dataArray['links'], // Pagination links
+            ],
+            'queryParams' => request()->all()
+        ]);
+    }
 }
