7d09e3ddec6c03be7db8f8fbc060abcd00b4f7a8
HidayatLahabu <hidayatdevacc@gmail.com>
2024-08-22 10:20:11 +0800

    udpate location pages

--- resources/js/Pages/Satusehat/Location/Index.jsx
+++ resources/js/Pages/Satusehat/Location/Index.jsx
@@ -86,7 +86,7 @@ export default function Index({ auth, dataTable, queryParams = {} }) {
                                         )}
                                     </tbody>
                                 </table>
-                                <Pagination links={location.links} />
+                                <Pagination links={dataTable.links} />
                             </div>
                         </div>
                     </div>
