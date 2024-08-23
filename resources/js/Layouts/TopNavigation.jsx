21cb5562c22c010d6caf5a32cb7eddea1daeb080
HidayatLahabu <hidayatdevacc@gmail.com>
2024-08-23 15:35:12 +0800

    update top navigtion

--- resources/js/Layouts/TopNavigation.jsx
+++ resources/js/Layouts/TopNavigation.jsx
@@ -8,7 +8,7 @@ const TopNavigation = ({ user }) => {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
 
     return (
-        <nav className="bg-white dark:bg-indigo-950 border-b border-gray-100 dark:border-gray-700 fixed top-0 left-0 right-0 z-10">
+        <nav className="bg-white dark:bg-indigo-950 border-b border-amber-400 dark:border-amber-400 fixed top-0 left-0 right-0 z-10">
             <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16">
                     <div className="flex">
