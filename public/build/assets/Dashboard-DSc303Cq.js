f4407ce5134812739215962076d54d86d98435a1
HidayatLahabu <hidayatdevacc@gmail.com>
2024-08-25 18:30:16 +0800

    update encounter detail

---
+++ public/build/assets/Dashboard-DSc303Cq.js
@@ -0,0 +1,1 @@
+import{j as r,Y as e}from"./app-CwPKJ98S.js";import{A as t}from"./AuthenticatedLayout-BiIx4KL9.js";import o from"./DashboardTable-DjdrrpH7.js";import"./transition-CGFcDsUV.js";function x({auth:s,items:a}){return r.jsxs(t,{user:s.user,children:[r.jsx(e,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(o,{items:a})})]})}export{x as default};
--- public/build/assets/Dashboard-ISMBUPC1.js
+++
@@ -1,1 +0,0 @@
-import{j as r,Y as e}from"./app-Bqw7grWU.js";import{A as t}from"./AuthenticatedLayout-BsErKOJM.js";import o from"./DashboardTable-CLT9tbyc.js";import"./transition-BuQCsFnG.js";function x({auth:s,items:a}){return r.jsxs(t,{user:s.user,children:[r.jsx(e,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(o,{items:a})})]})}export{x as default};
