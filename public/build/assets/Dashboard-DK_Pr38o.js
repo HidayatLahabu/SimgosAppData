2bb065f036e139b972924bed724fbc4379ad8716
HidayatLahabu <hidayatdevacc@gmail.com>
2024-08-26 09:38:03 +0800

    update detail location

---
+++ public/build/assets/Dashboard-DK_Pr38o.js
@@ -0,0 +1,1 @@
+import{j as r,Y as e}from"./app-9tOzbBWM.js";import{A as t}from"./AuthenticatedLayout-Cuc08jQk.js";import o from"./DashboardTable-CUFgoA_L.js";import"./transition-p47Kc97r.js";function x({auth:s,items:a}){return r.jsxs(t,{user:s.user,children:[r.jsx(e,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(o,{items:a})})]})}export{x as default};
