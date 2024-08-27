9e45f75c79884740ca4779e2f9e80e7dec79bc46
HidayatLahabu <hidayatdevacc@gmail.com>
2024-08-28 01:06:24 +0800

    build paitent update

---
+++ public/build/assets/Dashboard-2gyxs60o.js
@@ -0,0 +1,1 @@
+import{j as r,Y as e}from"./app-D5Svf3k5.js";import{A as t}from"./AuthenticatedLayout-D4_tyuuI.js";import o from"./DashboardTable-tIZY4qLN.js";import"./transition-Ch5KPWC0.js";function x({auth:s,items:a}){return r.jsxs(t,{user:s.user,children:[r.jsx(e,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(o,{items:a})})]})}export{x as default};
