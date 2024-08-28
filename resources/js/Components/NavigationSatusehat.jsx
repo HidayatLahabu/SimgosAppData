a7e10a80a3b549f494d771423a157c78c8c18bc9
HidayatLahabu <hidayatdevacc@gmail.com>
2024-08-29 00:52:57 +0800

    update satusehat nav

--- resources/js/Components/NavigationSatusehat.jsx
+++ resources/js/Components/NavigationSatusehat.jsx
@@ -55,109 +55,109 @@ export default function NavigationSatusehat() {
                 SatuSehat
             </NavLink>
             {isDropdownOpen && (
-                <div className="absolute dark:bg-indigo-900 text-white shadow-md mt-2 rounded-lg py-2 px-1 w-48">
+                <div className="absolute dark:bg-indigo-900 text-white shadow-md mt-2 rounded-lg py-2 px-1 w-96 grid grid-cols-2 gap-2">
                     <NavLink
                         href={route('organization.index')}
                         active={route().current('organization.index')}
-                        className="flex justify-between items-center px-4 py-2 mb-1 w-full"
+                        className="flex justify-between items-center px-4 py-2 w-full"
                     >
                         Organization
                     </NavLink>
                     <NavLink
                         href={route('location.index')}
                         active={route().current('location.index')}
-                        className="flex justify-between items-center px-4 py-2 mb-1 w-full"
+                        className="flex justify-between items-center px-4 py-2 w-full"
                     >
                         Location
                     </NavLink>
                     <NavLink
                         href={route('patient.index')}
                         active={route().current('patient.index')}
-                        className="flex justify-between items-center px-4 py-2 mb-1 w-full"
+                        className="flex justify-between items-center px-4 py-2 w-full"
                     >
                         Patient
                     </NavLink>
                     <NavLink
                         href={route('practitioner.index')}
                         active={route().current('practitioner.index')}
-                        className="flex justify-between items-center px-4 py-2 mb-1 w-full"
+                        className="flex justify-between items-center px-4 py-2 w-full"
                     >
                         Practitioner
                     </NavLink>
                     <NavLink
                         href={route('encounter.index')}
                         active={route().current('encounter.index')}
-                        className="flex justify-between items-center px-4 py-2 mb-1 w-full"
+                        className="flex justify-between items-center px-4 py-2 w-full"
                     >
                         Encounter
                     </NavLink>
                     <NavLink
                         href={route('condition.index')}
                         active={route().current('condition.index')}
-                        className="flex justify-between items-center px-4 py-2 mb-1 w-full"
+                        className="flex justify-between items-center px-4 py-2 w-full"
                     >
                         Condition
                     </NavLink>
                     <NavLink
                         href={route('observation.index')}
                         active={route().current('observation.index')}
-                        className="flex justify-between items-center px-4 py-2 mb-1 w-full"
+                        className="flex justify-between items-center px-4 py-2 w-full"
                     >
                         Observation
                     </NavLink>
                     <NavLink
                         href={route('procedure.index')}
                         active={route().current('procedure.index')}
-                        className="flex justify-between items-center px-4 py-2 mb-1 w-full"
+                        className="flex justify-between items-center px-4 py-2 w-full"
                     >
                         Procedure
                     </NavLink>
                     <NavLink
                         href={route('composition.index')}
                         active={route().current('composition.index')}
-                        className="flex justify-between items-center px-4 py-2 mb-1 w-full"
+                        className="flex justify-between items-center px-4 py-2 w-full"
                     >
                         Composition
                     </NavLink>
                     <NavLink
                         href={route('consent.index')}
                         active={route().current('consent.index')}
-                        className="flex justify-between items-center px-4 py-2 mb-1 w-full"
+                        className="flex justify-between items-center px-4 py-2 w-full"
                     >
                         Consent
                     </NavLink>
                     <NavLink
                         href={route('diagnosticReport.index')}
                         active={route().current('diagnosticReport.index')}
-                        className="flex justify-between items-center px-4 py-2 mb-1 w-full"
+                        className="flex justify-between items-center px-4 py-2 w-full"
                     >
-                        DiagnostiC Report
+                        Diagnostic Report
                     </NavLink>
                     <NavLink
                         href={route('medication.index')}
                         active={route().current('medication.index')}
-                        className="flex justify-between items-center px-4 py-2 mb-1 w-full"
+                        className="flex justify-between items-center px-4 py-2 w-full"
                     >
                         Medication
                     </NavLink>
                     <NavLink
                         href={route('medicationDispanse.index')}
                         active={route().current('medicationDispanse.index')}
-                        className="flex justify-between items-center px-4 py-2 mb-1 w-full"
+                        className="flex justify-between items-center px-4 py-2 w-full"
                     >
                         Medication Dispanse
                     </NavLink>
                     <NavLink
                         href={route('medicationRequest.index')}
                         active={route().current('medicationRequest.index')}
-                        className="flex justify-between items-center px-4 py-2 mb-1 w-full"
+                        className="flex justify-between items-center px-4 py-2 w-full"
                     >
                         Medication Request
                     </NavLink>
                     <NavLink
                         href={route('serviceRequest.index')}
                         active={route().current('serviceRequest.index')}
-                        className="flex justify-between items-center px-4 py-2 mb-1 w-full"
+                        className="flex justify-between items-center px-4 py-2 w-full"
                     >
                         Service Request
                     </NavLink>
