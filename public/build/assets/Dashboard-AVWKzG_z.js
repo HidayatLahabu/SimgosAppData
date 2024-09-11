82fa49e38daa4421316f5fbc689790728d2f2a72
HidayatLahabu <hidayatdevacc@gmail.com>
2024-09-11 16:26:36 +0800

    update pendaftaran & kunjungan

---
+++ public/build/assets/Dashboard-AVWKzG_z.js
@@ -0,0 +1,1 @@
+import{j as r,Y as e}from"./app-D8UT8J42.js";import{A as t}from"./AuthenticatedLayout-BL8Bvmqm.js";import o from"./DashboardTable-CezRkNFT.js";import"./transition-BddRKTFs.js";function x({auth:s,items:a}){return r.jsxs(t,{user:s.user,children:[r.jsx(e,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(o,{items:a})})]})}export{x as default};
