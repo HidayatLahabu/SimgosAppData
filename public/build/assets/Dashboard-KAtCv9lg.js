bb4384700336110642a67788b73fb39e789635e9
HidayatLahabu <hidayatdevacc@gmail.com>
2024-09-11 19:50:36 +0800

    update encounter

---
+++ public/build/assets/Dashboard-KAtCv9lg.js
@@ -0,0 +1,1 @@
+import{j as r,Y as e}from"./app-DbOcrO4k.js";import{A as t}from"./AuthenticatedLayout-BfaqUdKV.js";import o from"./DashboardTable-HxUSXcHc.js";import"./transition-BLBjtYFb.js";function x({auth:s,items:a}){return r.jsxs(t,{user:s.user,children:[r.jsx(e,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(o,{items:a})})]})}export{x as default};
