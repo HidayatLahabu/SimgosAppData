22256e86fac6d74710a8dc23f00ffb058e3effd0
HidayatLahabu <hidayatdevacc@gmail.com>
2024-09-16 12:31:51 +0000

    update component button detail

--- public/build/assets/Dashboard-DjV4ucK7.js
+++
@@ -1,1 +0,0 @@
-import{j as r,Y as e}from"./app-XNBS0g4z.js";import{A as t}from"./AuthenticatedLayout-PZ2ZL-eU.js";import o from"./DashboardTable-qUfEQfpo.js";import"./transition-BP8t3PEb.js";function x({auth:s,items:a}){return r.jsxs(t,{user:s.user,children:[r.jsx(e,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(o,{items:a})})]})}export{x as default};
