3da991afab64f90fca49e00b1db281553c0fb38b
HidayatLahabu <hidayatdevacc@gmail.com>
2024-10-12 02:29:43 +0800

    build after add laporan menu

--- public/build/assets/Dashboard-CNRv_ZZA.js
+++
@@ -1,1 +0,0 @@
-import{j as r,Y as a}from"./app-CqReR3qw.js";import{A as s}from"./AuthenticatedLayout-D7vnRERi.js";import i from"./SatuSehatTable-C2wdXOcf.js";import"./transition-DKucmMhV.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
---
+++ public/build/assets/Dashboard-DjYz4-Ol.js
@@ -0,0 +1,1 @@
+import{j as r,Y as a}from"./app-BE4pyFMl.js";import{A as s}from"./AuthenticatedLayout-BYfpbJZF.js";import i from"./SatuSehatTable-D9-rwCcG.js";import"./transition-p0GUiFmU.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
