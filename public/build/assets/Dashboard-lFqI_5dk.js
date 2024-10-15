fb5223c4094d18cbee79c898df802602e0185a7d
HidayatLahabu <hidayatdevacc@gmail.com>
2024-10-15 15:30:07 +0800

    build satusehat nik sufflenumber

--- public/build/assets/Dashboard-lFqI_5dk.js
+++
@@ -1,1 +0,0 @@
-import{j as r,Y as a}from"./app-qLT8BjmS.js";import{A as s}from"./AuthenticatedLayout-x-TOOrZA.js";import i from"./SatuSehatTable-CJ6wl-9w.js";import"./transition-DASRkb--.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
