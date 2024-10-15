12e7e6fe0902c63adc9a6ea4a2c7126b9d674d4c
HidayatLahabu <hidayatdevacc@gmail.com>
2024-10-15 12:34:14 +0800

    update build allergy satusehat

--- public/build/assets/Dashboard-BBSchfZj.js
+++
@@ -1,1 +0,0 @@
-import{j as r,Y as a}from"./app-OT6xckej.js";import{A as s}from"./AuthenticatedLayout-BA-l8yo7.js";import i from"./SatuSehatTable-XYDZfsGX.js";import"./transition-gSZx4PDa.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
