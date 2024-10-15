d773ed2189ec7cbf4eb169971c8c37f8bcf269bd
HidayatLahabu <hidayatdevacc@gmail.com>
2024-10-15 16:56:21 +0800

    build after update suffle number

---
+++ public/build/assets/Dashboard-BWMefA-x.js
@@ -0,0 +1,1 @@
+import{j as r,Y as a}from"./app-JDjNYoAt.js";import{A as s}from"./AuthenticatedLayout-DQEb6_Bv.js";import i from"./SatuSehatTable-CxF4q4XX.js";import"./transition-SZR8hrOD.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
