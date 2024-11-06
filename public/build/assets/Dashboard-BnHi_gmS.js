fc304ff1dcede29c659bd966bf330d80192addd8
HidayatLahabu <hidayatdevacc@gmail.com>
2024-11-06 13:59:14 +0000

    update app.name

--- public/build/assets/Dashboard-BnHi_gmS.js
+++
@@ -1,1 +0,0 @@
-import{j as r,Y as a}from"./app-Dvlr_T_r.js";import{A as s}from"./AuthenticatedLayout-C05LCJn9.js";import i from"./SatuSehatTable-Dx2ukNad.js";import"./transition-C8nCtlCa.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
