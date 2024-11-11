6f721056814ff311d020e08fc1d4d1535008a580
HidayatLahabu <hidayatdevacc@gmail.com>
2024-11-11 06:44:27 +0000

    build update bpjs kunjungan

---
+++ public/build/assets/Dashboard-DR3shU5Y.js
@@ -0,0 +1,1 @@
+import{j as r,Y as a}from"./app-BzH23yRA.js";import{A as s}from"./AuthenticatedLayout-Cjj54v21.js";import i from"./SatuSehatTable-DZFnrmKS.js";import"./transition-BVkBzBbc.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
