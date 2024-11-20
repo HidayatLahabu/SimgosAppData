bdae9033fd2832397506ebd91092e139a9797c12
HidayatLahabu <hidayatdevacc@gmail.com>
2024-11-20 14:03:30 +0000

    build update layanan

---
+++ public/build/assets/Dashboard-CjXtq1Ni.js
@@ -0,0 +1,1 @@
+import{j as r,Y as a}from"./app-CgMGpBPA.js";import{A as s}from"./AuthenticatedLayout-DPTrRgv2.js";import i from"./SatuSehatTable-CctiS4Nl.js";import"./transition-CfHEB1_N.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
