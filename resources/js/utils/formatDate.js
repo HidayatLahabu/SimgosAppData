b6c43b3e80a02fb2bb5e9a74019c21b8f3226fe4
MHLDevAcc <hidayatdevacc@gmail.com>
2024-11-26 18:16:05 +0800

    update date utility

--- resources/js/utils/formatDate.js
+++ resources/js/utils/formatDate.js
@@ -1,5 +1,7 @@
 export function formatDate(dateString) {
-    const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
     const date = new Date(dateString);
-    return date.toLocaleDateString('id-ID', options);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
 }
