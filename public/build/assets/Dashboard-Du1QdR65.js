110eef8bccb37d327b8e5319bfe4e38eeca3b29a
HidayatLahabu <hidayatdevacc@gmail.com>
2024-11-26 05:58:46 +0000

    build update satusehat barang

---
+++ public/build/assets/Dashboard-Du1QdR65.js
@@ -0,0 +1,1 @@
+import{j as r,Y as a}from"./app-DePqbkop.js";import{A as s}from"./AuthenticatedLayout-DHFb87Se.js";import i from"./SatuSehatTable-BX_sidjt.js";import"./transition-BJM57z2c.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
