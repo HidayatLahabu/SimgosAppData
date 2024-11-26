ca239467aeead36525ea75940f6fcd83965a12f5
HidayatLahabu <hidayatdevacc@gmail.com>
2024-11-26 07:33:49 +0000

    build update pendaftaran konsul

---
+++ public/build/assets/Dashboard-DPecWDDQ.js
@@ -0,0 +1,1 @@
+import{j as r,Y as a}from"./app-BBZAXOVG.js";import{A as s}from"./AuthenticatedLayout-CiP6h9d5.js";import i from"./SatuSehatTable-DmG8KT3i.js";import"./transition-ME7C2IlJ.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
