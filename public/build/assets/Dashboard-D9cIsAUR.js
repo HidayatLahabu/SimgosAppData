ac23e0d2bdae0f28d19a025f73db0aedfa526fba
HidayatLahabu <hidayatdevacc@gmail.com>
2024-11-26 10:25:25 +0000

    build update pendaftaran antrian

---
+++ public/build/assets/Dashboard-D9cIsAUR.js
@@ -0,0 +1,1 @@
+import{j as r,Y as a}from"./app-Cndm40lP.js";import{A as s}from"./AuthenticatedLayout-BAbdGo8R.js";import i from"./SatuSehatTable-CF7DncsS.js";import"./transition-DPfovGfA.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
