1650d79628b90d8cad04eb31c6a02be43115a971
HidayatLahabu <hidayatdevacc@gmail.com>
2024-11-27 08:07:21 +0000

    build update layanan laboratorium detail

---
+++ public/build/assets/ButtonTime-Ck5Pski0.js
@@ -0,0 +1,2 @@
+import{q as o,j as n,a as s}from"./app-DoYvk2P-.js";function x({href:t,text:e="Hari Ini"}){const{url:r}=o(),a=r===t;return n.jsx(s,{href:t,className:`text-sm font-bold py-2 px-3 rounded 
+                ${a?"bg-green-500 text-gray-200":"bg-amber-400 hover:bg-blue-500 text-gray-800 hover:text-gray-200"}`,children:e})}export{x as B};
