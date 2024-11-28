452c6099332824b336c4f9244cca25e5007886d7
HidayatLahabu <hidayatdevacc@gmail.com>
2024-11-28 03:23:36 +0000

    build update pendaftaran detail

---
+++ public/build/assets/Dashboard-auKYegwi.js
@@ -0,0 +1,1 @@
+import{j as r,Y as a}from"./app-gEuthTpi.js";import{A as s}from"./AuthenticatedLayout-4-ej89yo.js";import i from"./SatuSehatTable-CmFnYAOo.js";import"./transition-7s_wWZhn.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
