511b87499ad24333df75c142192eacff31c3bf51
HidayatLahabu <hidayatdevacc@gmail.com>
2024-12-03 05:51:07 +0000

    build update bpjs monitoring rekon

---
+++ public/build/assets/Dashboard-xmpHh0Ht.js
@@ -0,0 +1,1 @@
+import{j as r,Y as a}from"./app-CemCwGxq.js";import{A as s}from"./AuthenticatedLayout-BjZYqFJV.js";import i from"./SatuSehatTable-BUW1VmC_.js";import"./transition-C-wbO1cf.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
