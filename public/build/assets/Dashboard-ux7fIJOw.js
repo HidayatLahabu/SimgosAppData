b799de2b10b5219c18d99bab721da0ef87f8e77a
HidayatLahabu <hidayatdevacc@gmail.com>
2024-12-03 06:50:41 +0000

    build update layanan menu

---
+++ public/build/assets/Dashboard-ux7fIJOw.js
@@ -0,0 +1,1 @@
+import{j as r,Y as a}from"./app-BH1Xn5CL.js";import{A as s}from"./AuthenticatedLayout-CNDWSZs4.js";import i from"./SatuSehatTable-0ke7nrDM.js";import"./transition-DjM1sfli.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
