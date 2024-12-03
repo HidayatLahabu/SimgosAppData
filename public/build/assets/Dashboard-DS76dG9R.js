a3618e4596706e7371307f8da1959e3517e35393
HidayatLahabu <hidayatdevacc@gmail.com>
2024-12-03 05:01:56 +0000

    build update satusehat menu

---
+++ public/build/assets/Dashboard-DS76dG9R.js
@@ -0,0 +1,1 @@
+import{j as r,Y as a}from"./app-C1wCeYVs.js";import{A as s}from"./AuthenticatedLayout-CTwJqCuK.js";import i from"./SatuSehatTable-CUpyQ28J.js";import"./transition-CRnySkS5.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
