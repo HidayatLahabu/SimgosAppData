f6a69cbfbb620a11c424cca3253e1a480cf3074d
HidayatLahabu <hidayatdevacc@gmail.com>
2024-12-03 07:22:27 +0000

    build update bpjs menu

---
+++ public/build/assets/Dashboard-Cmm4uqof.js
@@ -0,0 +1,1 @@
+import{j as r,Y as a}from"./app-OfefhYc0.js";import{A as s}from"./AuthenticatedLayout-Dzvlu33l.js";import i from"./SatuSehatTable-BlbUfHKI.js";import"./transition-Cp0Bp0pi.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
