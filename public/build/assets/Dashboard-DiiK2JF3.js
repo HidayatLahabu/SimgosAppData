cfe4085cf742c1dc10430f624d11d9fef56cb737
HidayatLahabu <hidayatdevacc@gmail.com>
2024-12-04 10:28:41 +0000

    build update layanan menu

--- public/build/assets/Dashboard-DiiK2JF3.js
+++
@@ -1,1 +0,0 @@
-import{j as r,Y as a}from"./app-BAapsLKb.js";import{A as s}from"./AuthenticatedLayout-BQYs31YB.js";import i from"./SatuSehatTable-Ct7tydRu.js";import"./transition-CK11B9b-.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
