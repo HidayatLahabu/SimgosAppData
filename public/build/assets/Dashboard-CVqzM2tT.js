4e6362915c2056eb4c4fa6bd34307737bcbc38c8
HidayatLahabu <hidayatdevacc@gmail.com>
2024-12-05 14:20:23 +0800

    build update pendaftaran menu

--- public/build/assets/Dashboard-CVqzM2tT.js
+++
@@ -1,1 +0,0 @@
-import{j as r,Y as a}from"./app-CrfWcWvO.js";import{A as s}from"./AuthenticatedLayout-CnJLwKHV.js";import i from"./SatuSehatTable-BBLpd8MS.js";import"./transition-BXUh-MfD.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
---
+++ public/build/assets/Dashboard-Cxv_9To1.js
@@ -0,0 +1,1 @@
+import{j as r,Y as a}from"./app-CiID2avQ.js";import{A as s}from"./AuthenticatedLayout-oHWoXSyh.js";import i from"./SatuSehatTable-ID14j0KG.js";import"./transition-DXalChWR.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
