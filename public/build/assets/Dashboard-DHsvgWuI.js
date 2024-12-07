21f93b553988f8db5be0f99aed9e3ed0b7cb83b9
HidayatLahabu <hidayatdevacc@gmail.com>
2024-12-07 23:40:42 +0800

    build update frontend

--- public/build/assets/Dashboard-DHsvgWuI.js
+++
@@ -1,1 +0,0 @@
-import{j as r,Y as a}from"./app-BSnGz-ou.js";import{A as s}from"./AuthenticatedLayout-CCJ0PLmy.js";import i from"./SatuSehatTable-DcJm5ytb.js";import"./transition-BFK9tZOD.js";function x({auth:e,items:t}){return r.jsxs(s,{user:e.user,children:[r.jsx(a,{title:"Beranda"}),r.jsx("div",{className:"py-5 flex flex-wrap w-full",children:r.jsx(i,{items:t})})]})}export{x as default};
