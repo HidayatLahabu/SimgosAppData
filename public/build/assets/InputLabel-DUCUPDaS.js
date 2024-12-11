d2a729be69b8d6359455901d3dac15ac04eb5bf0
HidayatLahabu <hidayatdevacc@gmail.com>
2024-12-11 12:00:39 +0800

    build update dashboard

--- public/build/assets/InputLabel-DUCUPDaS.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-s8l1-nAC.js";function n({value:t,className:e="",children:r,...a}){return s.jsx("label",{...a,className:"block font-medium text-sm text-gray-700 dark:text-gray-300 "+e,children:t||r})}export{n as I};
