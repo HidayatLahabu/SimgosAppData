a2bde77bafb78cbf47e002e16b26d30998a2df77
HidayatLahabu <hidayatdevacc@gmail.com>
2025-01-06 13:54:41 +0800

    build update laporan RL 1.2

---
+++ public/build/assets/InputError-C_wZpXOR.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-D6HFrM9Y.js";function o({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:"text-sm text-red-600 dark:text-red-400 "+t,children:r}):null}export{o as I};
