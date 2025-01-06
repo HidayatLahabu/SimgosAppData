fe7a4ee61ec4763d47409fababf9d319e2179358
MHLDevAcc <hidayatdevacc@gmail.com>
2025-01-06 13:52:23 +0800

    update laporan RL 1.2

--- app/Http/Controllers/Laporan/LaporanRl12Controller.php
+++ app/Http/Controllers/Laporan/LaporanRl12Controller.php
@@ -11,20 +11,30 @@ class LaporanRl12Controller extends Controller
 {
     public function index(Request $request)
     {
-        // Tanggal awal bulan berjalan
+        // Tanggal awal bulan berjalan (Current year)
         $tgl_awal = Carbon::now()->startOfYear()->toDateString();
-
-        // Tanggal hari ini
+        // Tanggal hari ini (Current date)
         $tgl_akhir = Carbon::now()->toDateString();
 
-        // Panggil prosedur yang telah dibuat
-        $data = DB::connection('mysql10')->select('CALL laporan.LaporanRL12(?, ?)', [$tgl_awal, $tgl_akhir]);
+        // Tanggal awal bulan berjalan (Previous year)
+        $tgl_awal_last_year = Carbon::now()->subYear()->startOfYear()->toDateString();
+        // Tanggal akhir bulan berjalan (Previous year)
+        $tgl_akhir_last_year = Carbon::now()->subYear()->endOfYear()->toDateString();
 
-        //dd($data);
+        // Panggil prosedur yang telah dibuat untuk current year
+        $data_current_year = DB::connection('mysql10')->select('CALL laporan.LaporanRL12(?, ?)', [$tgl_awal, $tgl_akhir]);
+
+        // Panggil prosedur yang telah dibuat untuk previous year
+        $data_last_year = DB::connection('mysql10')->select('CALL laporan.LaporanRL12(?, ?)', [$tgl_awal_last_year, $tgl_akhir_last_year]);
+
+        // Return data to frontend
         return inertia("Laporan/Rl12/Index", [
-            'items' => $data,
+            'items_current_year' => $data_current_year,
+            'items_last_year' => $data_last_year,
             'tgl_awal' => $tgl_awal,
             'tgl_akhir' => $tgl_akhir,
+            'tgl_awal_last_year' => $tgl_awal_last_year,
+            'tgl_akhir_last_year' => $tgl_akhir_last_year,
         ]);
     }
 }
--- resources/js/Pages/Laporan/Rl12/Index.jsx
+++ resources/js/Pages/Laporan/Rl12/Index.jsx
@@ -3,137 +3,29 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import { formatDate } from '@/utils/formatDate';
 import { formatNumber } from '@/utils/formatNumber';
+import ThisYear from './ThisYear';
+import LastYear from './LastYear';
 
-export default function LaporanRl12({ auth, items = [], tgl_awal, tgl_akhir }) {
-
-    if (!Array.isArray(items)) {
-        console.error('Expected items to be an array but received:', items);
-        return <div>Error: Data not available</div>;
-    }
-
-    const data = items[0] || {};
-
-    const fieldMap = {
-        KODERS: "Kode Rumah Sakit",
-        NAMAINST: "Nama Instansi",
-        KODEPROP: "Kode Provinsi",
-        KOTA: "Kota/Kabupaten",
-        TAHUN: "Tahun",
-        AWAL: "Jumlah Awal",
-        MASUK: "Jumlah Masuk",
-        PINDAHAN: "Pindahan",
-        DIPINDAHKAN: "Dipindahkan",
-        HIDUP: "Pasien Hidup",
-        MATI: "Pasien Mati",
-        MATIKURANG48: "Mati < 48 Jam",
-        MATILEBIH48: "Mati > 48 Jam",
-        LD: "Lama Dirawat",
-        SISA: "Sisa",
-        HP: "Hari Perawatan",
-        JMLTT: "Jumlah Tempat Tidur",
-        BOR: "Bed Occupancy Rate",
-        BORLK: "BOR Laki-laki",
-        BORPR: "BOR Perempuan",
-        AVLOS: "Average Length of Stay",
-        AVLOSLK: "AVLOS Laki-laki",
-        AVLOSPR: "AVLOS Perempuan",
-        BTO: "Bed Turn Over",
-        BTOLK: "BTO Laki-laki",
-        BTOPR: "BTO Perempuan",
-        TOI: "Turn Over Interval",
-        TOILK: "TOI Laki-laki",
-        TOIPR: "TOI Perempuan",
-        NDR: "Net Death Rate",
-        NDRLK: "NDR Laki-laki",
-        NDRPR: "NDR Perempuan",
-        GDR: "Gross Death Rate",
-        GDRLK: "GDR Laki-laki",
-        GDRPR: "GDR Perempuan",
-        JMLKUNJUNGAN: "Jumlah Kunjungan",
-    };
-
-    const sections = {
-        "Informasi Umum": ["KODERS", "NAMAINST", "KODEPROP", "KOTA", "TAHUN", "JMLTT"],
-        "Data Pasien dan Perawatan": ["AWAL", "MASUK", "PINDAHAN", "DIPINDAHKAN", "HIDUP", "MATI", "MATIKURANG48", "MATILEBIH48", "NDR", "NDRLK", "NDRPR", "GDR", "GDRLK", "GDRPR", "LD"],
-        "Data Tempat Tidur dan BOR": ["BOR", "BORLK", "BORPR", "AVLOS", "AVLOSLK", "AVLOSPR", "BTO", "BTOLK", "BTOPR", "TOI", "TOILK", "TOIPR", "JMLKUNJUNGAN", "HP", "SISA"],
-    };
+export default function LaporanRl12({ auth, items_current_year, tgl_awal, tgl_akhir, items_last_year, tgl_awal_last_year, tgl_akhir_last_year }) {
 
     return (
         <AuthenticatedLayout user={auth.user}>
             <Head title="Laporan" />
 
-            <div className="py-5 flex flex-wrap w-full">
-                <div className="max-w-full mx-auto sm:px-5 lg:px-5 w-full">
-                    <div className="bg-white dark:bg-indigo-950 overflow-hidden shadow-sm sm:rounded-lg w-full">
-                        <div className="p-5 text-gray-900 dark:text-gray-100 w-full">
-                            <h1 className="uppercase text-center font-bold text-2xl pb-2">LAPORAN RL 1.2</h1>
-                            <p className="text-center pb-2">
-                                <strong>Periode Tanggal: </strong>{formatDate(tgl_awal)} - {formatDate(tgl_akhir)}
-                            </p>
-                            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                                <div className="bg-white dark:bg-indigo-950 rounded-lg shadow-lg p-2">
-
-                                    <table className="min-w-full table-auto mt-2 border border-gray-500 dark:border-gray-600">
-                                        <thead>
-                                            <tr>
-                                                <th colSpan="2" className="text-normal font-semibold uppercase px-2 py-1 text-yellow-500 dark:bg-indigo-900">
-                                                    Informasi Organisasi
-                                                </th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {sections["Informasi Umum"].map((key) => (
-                                                <tr key={key} className="hover:bg-gray-100 dark:hover:bg-indigo-800 border border-gray-500 dark:border-gray-600">
-                                                    <td className="px-4 py-1 font-medium border border-gray-500 dark:border-gray-600">{fieldMap[key]}</td>
-                                                    <td className="px-4 py-1">{data[key] || '-'}</td>
-                                                </tr>
-                                            ))}
-                                        </tbody>
-                                    </table>
-                                </div>
-                                <div className="bg-white dark:bg-indigo-950 rounded-lg shadow-lg p-2">
-                                    <table className="min-w-full table-auto mt-2 border border-gray-500 dark:border-gray-600">
-                                        <thead>
-                                            <tr>
-                                                <th colSpan="2" className="text-normal font-semibold uppercase px-2 py-1 text-yellow-500 dark:bg-indigo-900">
-                                                    Data Pasien dan Perawatan
-                                                </th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {sections["Data Pasien dan Perawatan"].map((key) => (
-                                                <tr key={key} className="hover:bg-gray-100 dark:hover:bg-indigo-800 border border-gray-500 dark:border-gray-600">
-                                                    <td className="px-4 py-1 font-medium border border-gray-500 dark:border-gray-600">{fieldMap[key]}</td>
-                                                    <td className="px-4 py-1">{typeof data[key] === 'number' ? formatNumber(data[key]) : data[key] || '-'}</td>
-                                                </tr>
-                                            ))}
-                                        </tbody>
-                                    </table>
-                                </div>
-                                <div className="bg-white dark:bg-indigo-950 rounded-lg shadow-lg p-2">
-                                    <table className="min-w-full table-auto mt-2 border border-gray-500 dark:border-gray-600">
-                                        <thead>
-                                            <tr>
-                                                <th colSpan="2" className="text-normal font-semibold uppercase px-2 py-1 text-yellow-500 dark:bg-indigo-900">
-                                                    Data Tempat Tidur dan Hunian
-                                                </th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {sections["Data Tempat Tidur dan BOR"].map((key) => (
-                                                <tr key={key} className="hover:bg-gray-100 dark:hover:bg-indigo-800 border border-gray-500 dark:border-gray-600">
-                                                    <td className="px-4 py-1 font-medium border border-gray-500 dark:border-gray-600">{fieldMap[key]}</td>
-                                                    <td className="px-4 py-1">{typeof data[key] === 'number' ? formatNumber(data[key]) : data[key] || '-'}</td>
-                                                </tr>
-                                            ))}
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div className="flex">
+                <ThisYear
+                    items_current_year={items_current_year}
+                    tgl_awal={tgl_awal}
+                    tgl_akhir={tgl_akhir}
+                />
+                <LastYear
+                    items_last_year={items_last_year}
+                    tgl_awal_last_year={tgl_awal_last_year}
+                    tgl_akhir_last_year={tgl_akhir_last_year}
+                />
             </div>
+
+
         </AuthenticatedLayout>
     );
 }
