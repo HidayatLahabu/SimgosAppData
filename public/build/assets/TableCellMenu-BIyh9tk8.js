0acb3d5993fb41e683c01c1d568261d6893b813c
HidayatLahabu <hidayatdevacc@gmail.com>
2025-01-08 00:12:18 +0800

    build update laporan RL 1.2

---
+++ public/build/assets/TableCellMenu-BIyh9tk8.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-C5z7uMvS.js";const r=({children:e})=>t.jsx("td",{className:"py-2 text-center flex justify-center items-center",children:e});export{r as T};
---
+++ public/build/assets/formatNumber-D_d3e_je.js
@@ -0,0 +1,1 @@
+function i(t){const m=new Intl.NumberFormat("id-ID",{minimumFractionDigits:2,maximumFractionDigits:2}).format(Math.abs(t));return t<0?`-${m}`:m}export{i as f};
