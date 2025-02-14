091e915e6698a61da92f46ce0893201bf31e70c3
HidayatLahabu <hidayatdevacc@gmail.com>
2025-02-14 18:28:42 +0800

    update layanan

--- public/build/assets/InputLabel-CyFQFDHF.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-W41N2evA.js";function n({value:t,className:e="",children:r,...a}){return s.jsx("label",{...a,className:"block font-medium text-sm text-gray-700 dark:text-gray-300 "+e,children:t||r})}export{n as I};
