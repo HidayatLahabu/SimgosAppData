820e01493118b1a88b2bd16fb8cb7994e28b8758
HidayatLahabu <hidayatdevacc@gmail.com>
2025-12-14 13:05:45 +0800

    update api

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Lis\OrderLabDetailController;
 
-// Route::get('/lis/order-lab-detail', [OrderLabDetailController::class, 'index']);
+Route::get('/lis/order-lab-detail', [OrderLabDetailController::class, 'index']);
 
-Route::get('/ping', function () {
-    return response()->json([
-        'status'  => 'ok',
-        'app'     => 'SIMGOS',
-        'message' => 'API LIS aktif',
-        'time'    => now()->toDateTimeString(),
-    ]);
-});
+// Route::get('/ping', function () {
+//     return response()->json([
+//         'status'  => 'ok',
+//         'app'     => 'SIMGOS',
+//         'message' => 'API LIS aktif',
+//         'time'    => now()->toDateTimeString(),
+//     ]);
+// });
